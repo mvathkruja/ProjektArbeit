f5025edaa3c7954dc418d6419498227702efbb0f
mvathkruja <vathkrujamaria@gmail.com>
2025-02-17 11:55:14 +0100

    Java Program for listing books

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        String jdbcUrl = "jdbc:postgresql://aws-0-eu-central-1.pooler.supabase.com:5432/postgres?user=postgres.rikskdytghgmivyurgyt";
+        String username = "postgres.rikskdytghgmivyurgyt";
+        String password = "1Schueler";
+
+        Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+
+        Statement statement = connection.createStatement();
+
+        ResultSet resultSet = statement.executeQuery("select * from books order by asc");
+
+        while (resultSet.next())
+        {
+            String columnValue = resultSet.getString("title");
+            System.out.println("Column Value: " + columnValue);
+        }
+
+    }
+}
